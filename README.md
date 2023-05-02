d41573f5b1794c7c02577e5b989fed930f8bf42b
urregozw <sebastian.garcia@loanlogics.com>
2023-05-01 21:37:45 -0500

    App como una API

--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 ```
 python3.8 -m pip install -r requirements.txt
 ```
+## Variables de entorno
+- GOOGLE_CLIENT_ID
+- GOOGLE_CLIENT_SECRET
--- app.py
+++ app.py
@@ -61,16 +61,9 @@ def load_user(user_id):
 @app.route("/")
 def index():
     if current_user.is_authenticated:
-        return (
-            "<p>Hello, {}! You're logged in! Email: {}</p>"
-            "<div><p>Google Profile Picture:</p>"
-            '<img src="{}" alt="Google profile pic"></img></div>'
-            '<a class="button" href="/logout">Logout</a>'.format(
-                current_user.name, current_user.email, current_user.profile_pic
-            )
-        )
+        return {"user": current_user.name, "email":current_user.email}
     else:
-        return '<a class="button" href="/login">Google Login</a>'
+        return redirect("https://localhost:5000/login")
 
 
 @app.route("/login")
